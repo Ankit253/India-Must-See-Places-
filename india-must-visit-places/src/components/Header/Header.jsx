6ebac0ee3f31a8c4e72ce5861b5aec938c35aa44
ankit253 <yadavankit.ay253@gmail.com>
2021-10-15 14:59:49 +0530

    setting up the map

--- india-must-visit-places/src/components/Header/Header.jsx
+++ india-must-visit-places/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search'
+import SearchIcon from '@material-ui/icons/Search';
 
 // import { mergeClasses } from '@material-ui/styles';
 import useStyles from './styles'
--- india-must-visit-places/src/components/Map/Map.jsx
+++ india-must-visit-places/src/components/Map/Map.jsx
@@ -1,11 +1,31 @@
 
 
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab'
+
+import useStyles from './styles';
 
 const Map=() => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates={lat:0 , lng:0};
     return (
-      <div >
-           <h1> Map</h1> 
+      <div className={classes.mapContainer}>
+           <GoogleMapReact
+             bootstrapURLKeys={{Key: 'AIzaSyBjfjsRgrD8hOK8gbHOy8QWA3v8kOXD4R4' }}
+             defaultCentre={coordinates}
+            center={coordinates}
+            defaultZoom={8}
+            margin={[50,50,50,50]}
+            option={''}
+            onChange={''}
+            onChildClick={''}
+
+           ></GoogleMapReact>
       </div>
     );
   }
